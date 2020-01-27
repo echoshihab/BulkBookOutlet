af7c326c5945768412939267ae7ab27ef6389aa8
shihab <echoshihab@gmail.com>
2020-01-26 21:21:54 -0500

    add and implement stord procedure repository

---
+++ BulkBookOutlet.DataAccess/Data/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using BulkBookOutlet.DataAccess.Data.Repository.IRepository;
+using BulkBookOutlet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BulkBookOutlet.DataAccess.Data.Repository
+{
+    class CategoryRepository: Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db): base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+           
+        }
+    }
+}
---
+++ BulkBookOutlet.DataAccess/Data/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using BulkBookOutlet.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkBookOutlet.DataAccess.Data.Repository.IRepository
+{
+    interface ICategoryRepository: IRepository<Category>
+    {
+        void Update(Category category);
+
+    }
+}
