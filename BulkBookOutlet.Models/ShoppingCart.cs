efe0b93881803e79dd8bd4f64583a98f22678f6a
shihab <echoshihab@gmail.com>
2020-02-12 21:48:42 -0500

    Add shopping cart model

---
+++ BulkBookOutlet.Models/ShoppingCart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BulkBookOutlet.Models
+{
+    public class ShoppingCart
+    {
+
+        public ShoppingCart()
+        {
+            Count = 1;
+        }
+        [Key]
+        public int ID { get; set; }
+
+        public int ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+
+        public Product Product { get; set; }
+
+        [Range(1,1000,ErrorMessage ="Please enter a value between 1 and 1000")]
+
+        public int Count { get; set; }
+        [NotMapped]
+        public double Price { get; set; }
+
+    }
+}
