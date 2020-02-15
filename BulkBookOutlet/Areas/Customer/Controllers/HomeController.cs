a97fe81ac01f98a2b8a9151bbbfabf312d7e7fa5
shihab <echoshihab@gmail.com>
2020-02-14 19:57:07 -0500

    display products with images prices on home page

--- BulkBookOutlet/Areas/Customer/Controllers/HomeController.cs
+++ BulkBookOutlet/Areas/Customer/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BulkBookOutlet.Models.ViewModels;
+using BulkBookOutlet.DataAccess.Data.Repository.IRepository;
+using BulkBookOutlet.Models;
 
 namespace BulkBookOutlet.Areas.Customer.Controllers
 {
@@ -13,15 +15,19 @@ namespace BulkBookOutlet.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+
+            return View(productList);
         }
 
         public IActionResult Privacy()
