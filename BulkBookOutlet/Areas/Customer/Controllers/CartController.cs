37721ca06581b28feca7a19315a256ce6dd7d9d2
shihab <echoshihab@gmail.com>
2020-02-17 14:29:54 -0500

    add logic resending authorized comany confirmation email

--- BulkBookOutlet/Areas/Customer/Controllers/CartController.cs
+++ BulkBookOutlet/Areas/Customer/Controllers/CartController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using BulkBookOutlet.DataAccess.Data.Repository.IRepository;
 using BulkBookOutlet.Models.ViewModels;
@@ -9,6 +11,7 @@ using BulkBookOutlet.Utility;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace BulkBookOutlet.Areas.Customer.Controllers
 {
@@ -58,5 +61,34 @@ namespace BulkBookOutlet.Areas.Customer.Controllers
 
             return View(ShoppingCartVM);
         }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public async Task<IActionResult> IndexPOST()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var user = _unitOfWork.User.GetFirstOrDefault(u => u.Id == claim.Value);
+
+            if(user == null)
+            {
+                ModelState.AddModelError(string.Empty, "Verification email is empty!");
+            }
+
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var callbackUrl = Url.Page(
+                "/Account/ConfirmEmail",
+                pageHandler: null,
+                values: new { area = "Identity", userId = user.Id, code = code },
+                protocol: Request.Scheme);
+
+            await _emailSender.SendEmailAsync(user.Email, "Confirm your email",
+                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                ModelState.AddModelError(string.Empty, "Verification email sent. Please check your email");
+            return RedirectToAction("Index");
+        }
+
     }
 }
