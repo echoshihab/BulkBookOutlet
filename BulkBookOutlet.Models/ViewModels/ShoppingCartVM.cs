3c7832c4a789ace010f7b53949421fb4efac264e
shihab <echoshihab@gmail.com>
2020-02-17 12:49:09 -0500

    add shopping cart controller + index action

---
+++ BulkBookOutlet.Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkBookOutlet.Models.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public IEnumerable<ShoppingCart> ListCart { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+
+    }
+}
