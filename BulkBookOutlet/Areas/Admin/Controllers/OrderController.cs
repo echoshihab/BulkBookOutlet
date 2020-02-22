66d6f3b09b19703868f191e52156fbdd761a6c12
shihab <echoshihab@gmail.com>
2020-02-22 10:35:41 -0500

    add order controller

---
+++ BulkBookOutlet/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkBookOutlet.DataAccess.Data.Repository.IRepository;
+using BulkBookOutlet.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkBookOutlet.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+
+        [HttpGet]
+        public IActionResult GetOrderList()
+        {
+            IEnumerable<OrderHeader> orderHeaderList;
+            orderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            return Json(new { data = orderHeaderList });
+        }
+        #endregion
+
+
+    }
+
+
+
+}
